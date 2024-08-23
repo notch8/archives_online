9a17235a5b4177ac4a1db023dde5a0a6c21b5b76
Kirk Wang <kirk.wang@scientist.com>
2024-08-23 15:19:34 -0700

    🎁 Add campus to facets
    This will render the campus in the facets field.  All of the changes
    came from the current ngao app.
    
    Ref:
    - https://github.com/scientist-softserv/archives_online/issues/19

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
   def additional_locale_routing_scopes
     [blacklight, arclight_engine]
   end
+
+  def render_campus_name(args)
+    key = args[:value] || []
+    convert_campus_id(key[0])
+  end
+
+  def render_campus_facet(options = {})
+    convert_campus_id(options)
+  end
 end
