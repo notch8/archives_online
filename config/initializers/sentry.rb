974fb85be30a67e58a8f19fc0a81fc3da473f6db
April Rieger <april.rieger@yahoo.com>
2025-04-11 02:20:05 -0700

    Update for rubocop

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,7 +5,7 @@ require 'sentry-ruby'
 
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
-  config.breadcrumbs_logger = %i[:active_support_logger, :http_logger, :sentry_logger]
+  config.breadcrumbs_logger = %i[active_support_logger http_logger sentry_logger]
   config.environment = ENV['SENTRY_ENVIRONMENT']
   config.debug = true
 end
