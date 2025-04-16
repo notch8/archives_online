f10276b4c082d485fccfdce057afe4d52e890203
Kirk Wang <kirk.wang@scientist.com>
2025-04-16 14:33:07 -0700

    🚧 Configure the printable view for downloads
    This commit will bring in some changes to make the printable view show
    up.  It is a WIP because currently we have nothing to download.
    
    Ref:
    - https://github.com/notch8/archives_online/issues/111

--- app/components/ngao/arclight/sidebar_component.rb
+++ app/components/ngao/arclight/sidebar_component.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 # OVERRIDE Arclight v1.4.0 to add collection_id to the collection context
+#   and to render the `Ngao::Arclight::DocumentDownloadComponent`
 
 module Ngao
   module Arclight
     class SidebarComponent < ::Arclight::SidebarComponent
       def collection_context
-        render Ngao::Arclight::CollectionContextComponent.new(presenter: document_presenter(document),
-                                                              download_component: ::Arclight::DocumentDownloadComponent)
+        render Ngao::Arclight::CollectionContextComponent.new(
+          presenter: document_presenter(document),
+          download_component: Ngao::Arclight::DocumentDownloadComponent
+        )
       end
     end
   end
